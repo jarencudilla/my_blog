de2bda85c44e46823d0026a72bae4e4b1b5214c9
jarencudilla <jarencudilla@gmail.com>
2021-05-14 20:00:40 +0800

    update Blog app

--- db/migrate/20210512131454_create_articles.rb
+++ db/migrate/20210512131454_create_articles.rb
@@ -2,6 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :name
+      t.string :author
       t.text :body
 
       t.timestamps
